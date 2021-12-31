c6f490c76a8826fa6cec6fdf802677134aaac61b
unknown <ariel@klikotty.com>
2021-12-31 13:36:00 +0200

    dashboard ready

--- Assets/Scripts/Data/Serializables/WizardStatsData.cs
+++ Assets/Scripts/Data/Serializables/WizardStatsData.cs
@@ -11,21 +11,71 @@ public class WizardStatsData
     public DefenceStatsData DefenceStatsData;
     public OrbStatsData OrbStatsData;
     public ManaStatsData ManaStatsData;
+
+    public void WriteWizardStats(){
+        WizardStats wizardStatsObject = GameObject.Find("WizardStats").GetComponent<WizardStats>();
+        wizardStatsObject.SetDmg(AttackStatsData.MinBaseDamage, AttackStatsData.MaxBaseDamage);
+        wizardStatsObject.SetCritDmg(AttackStatsData.CriticalDmg);
+        wizardStatsObject.SetCritRate(AttackStatsData.CriticalRate);
+        wizardStatsObject.SetArmorPenetration(AttackStatsData.ArmorPenetration);
+        wizardStatsObject.SetMaxHP(DefenceStatsData.MaxHP);
+        wizardStatsObject.SetRecovery(DefenceStatsData.Recovery);
+        wizardStatsObject.SetMirroring(DefenceStatsData.Mirroring);
+        wizardStatsObject.SetAvoidability(DefenceStatsData.Avoidability);
+        wizardStatsObject.SetMaxMana(ManaStatsData.MaxMana);
+        wizardStatsObject.SetStartMana(ManaStatsData.StartMana);
+        wizardStatsObject.SetManaRegeneration(ManaStatsData.ManaRegeneration);
+        wizardStatsObject.SetPassiveManaRegeneration(ManaStatsData.PassiveManaRegeneration);
+    }
+
+    public void EquipItem(InventoryItem equipedItem) {
+
+        UpdateAttackStatsData(equipedItem.GetAttackStatsData(), 1);
+        UpdateDefenceStatsData(equipedItem.GetDefenceStatsData(), 1);
+        UpdateManaStatsData(equipedItem.GetManaStatsData(), 1);
+    }
+
+    public void RemoveItem(InventoryItem removedItem) {
+        UpdateAttackStatsData(removedItem.GetAttackStatsData(), -1);
+        UpdateDefenceStatsData(removedItem.GetDefenceStatsData(), -1);
+        UpdateManaStatsData(removedItem.GetManaStatsData(), -1);
+    }
+
+    private void UpdateAttackStatsData(AttackStatsData equipedItem, int factor){
+        AttackStatsData.CriticalDmg += (equipedItem.CriticalDmg*factor);
+        AttackStatsData.CriticalRate += (equipedItem.CriticalRate*factor);
+        AttackStatsData.MinBaseDamage += (equipedItem.MinBaseDamage*factor);
+        AttackStatsData.MaxBaseDamage += (equipedItem.MaxBaseDamage*factor);
+        AttackStatsData.ArmorPenetration += (equipedItem.ArmorPenetration*factor);
+    }
+    private void UpdateDefenceStatsData(DefenceStatsData equipedItem, int factor){
+        DefenceStatsData.MaxHP += (equipedItem.MaxHP*factor);
+        DefenceStatsData.Recovery += (equipedItem.Recovery*factor);
+        DefenceStatsData.Mirroring += (equipedItem.Mirroring*factor);
+        DefenceStatsData.Avoidability += (equipedItem.Avoidability*factor);
+    }
+    private void UpdateManaStatsData(ManaStatsData equipedItem, int factor){
+        ManaStatsData.MaxMana += (equipedItem.MaxMana*factor);
+        ManaStatsData.StartMana += (equipedItem.StartMana*factor);
+        ManaStatsData.ManaRegeneration += (equipedItem.ManaRegeneration*factor);
+        ManaStatsData.PassiveManaRegeneration += (equipedItem.PassiveManaRegeneration*factor);
+    }
 }
 
 [Serializable]
 public class AttackStatsData
 { 
-    public int MinBaseDamage = 0;
-    public int MaxBaseDamage = 0;
+    public int MinBaseDamage = 1;
+    public int MaxBaseDamage = 3;
     public float CriticalRate = 0;
+    public float CriticalDmg = 0;
     public float ArmorPenetration = 0;
 }
 
 [Serializable]
 public class DefenceStatsData
 { 
-    public int MaxHP = 0;
+    public int MaxHP = 50;
     public int Recovery = 0;
     public float Avoidability = 0;
     public float Mirroring = 0;
@@ -34,9 +84,9 @@ public class DefenceStatsData
 [Serializable]
 public class ManaStatsData
 { 
-    public int MaxMana = 0;
+    public int MaxMana = 30;
     public float StartMana = 0;
-    public int ManaRegeneration = 0;
+    public int ManaRegeneration = 5;
     public int PassiveManaRegeneration = 0;
 }
 
