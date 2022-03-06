e1b9f8a8ef1b4c87efe5f1222bc9441dfe4157d7
rollerbit-developer <developer@rollerbit.app>
2022-03-06 16:55:40 +0200

    change gamemodes modal

--- Assets/Scripts/Game/DecisionManager.cs
+++ Assets/Scripts/Game/DecisionManager.cs
@@ -57,7 +57,6 @@ public class DecisionManager : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
             {
-                Debug.Log(OpponentId);
                 if (hit.transform.gameObject.name == "Opponent" && !IsDecisionMakingOver())
                 {
                     OpponentId = hit.transform.gameObject.GetComponent<Wizard>().wizardId;
