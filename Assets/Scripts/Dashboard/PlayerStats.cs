e8c4c3338fe80788e0202ba72e5c4954b99c0d82
rollerbit-developer <developer@rollerbit.app>
2022-05-05 21:53:14 +0300

    adding new style for playerStats

--- Assets/Scripts/Dashboard/PlayerStats.cs
+++ Assets/Scripts/Dashboard/PlayerStats.cs
@@ -9,9 +9,6 @@ public class PlayerStats : MonoBehaviour
     [SerializeField] TMP_Text _energyInput;
     [SerializeField] TMP_Text _XPInput;
     [SerializeField] Slider _levelBar;
-    [SerializeField] GameObject _settings;
-    [SerializeField] GameObject _musicCheckMark;
-    [SerializeField] GameObject _sfxCheckMark;
 
     private PlayerStatsController _playerStatsController = new PlayerStatsController();
 
@@ -36,61 +33,9 @@ public class PlayerStats : MonoBehaviour
     {
         PlayerStatsData playerStatsData = _playerStatsController.GetPlayerStatsData();
         _coinsInput.text = playerStatsData.GetCoins() + "";
-        _XPInput.text = playerStatsData.GetXP() + "";
+        _XPInput.text = playerStatsData.GetLevelPoints() + "/" + "100";
         _energyInput.text = playerStatsData.GetCrystals() + "/" + playerStatsData.GetMaxCrystals();
-        _levelInput.text = "level: " + playerStatsData.GetLevel();
+        _levelInput.text = "" + playerStatsData.GetLevel();
         _levelBar.value = playerStatsData.GetLevelPoints();
-    }
-
-    private void toggleSoundCheckMarks()
-    {
-        if(SoundManager.Instance.isMusicOn)
-        {
-            _musicCheckMark.SetActive(true);
-        }
-        else
-        {
-            _musicCheckMark.SetActive(false);
-        }
-
-        if(SoundManager.Instance.isSfxOn)
-        {
-            _sfxCheckMark.SetActive(true);
-        }
-        else
-        {
-            _sfxCheckMark.SetActive(false);
-        }
-    }
-
-    public void ToggleSettings()
-    {
-        if (_settings.activeSelf)
-        {
-            _settings.SetActive(false);
-        }
-        else
-        {
-            _settings.SetActive(true);
-        }
-        toggleSoundCheckMarks();
-    }
-
-    public void toggleMusic()
-    {
-        SoundManager.Instance.isMusicOn = !SoundManager.Instance.isMusicOn;
-        if(SoundManager.Instance.isMusicOn)
-        {
-            SoundManager.Instance.PlayGameThemeSound();
-        }
-        else 
-        {
-            SoundManager.Instance.StopGameThemeSound();
-        }
-    }
-
-    public void toggleSfx()
-    {
-        SoundManager.Instance.isSfxOn = !SoundManager.Instance.isSfxOn;
     }
 }
